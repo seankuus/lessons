10ee7d9d9691ff3945a11ed1ec6f6ce751386f05
Sean Kuusinen <seankuusinen@gmail.com>
2020-06-02 17:58:44 +0000

    Object.entries allows you to access both keys and values within the same array.

---
+++ lesson_5/problem6.js
@@ -0,0 +1,15 @@
+let munsters = {
+  herman: { age: 32, gender: 'male' },
+  lily: { age: 30, gender: 'female' },
+  grandpa: { age: 402, gender: 'male' },
+  eddie: { age: 10, gender: 'male' },
+  marilyn: { age: 23, gender: 'female'}
+};
+
+Object.entries(munsters).forEach(entry => {
+  let name = entry[0];
+  let age = entry[1]['age'];
+  let gender = entry[1].gender;
+
+  console.log(`${name} is a ${age}-year-old ${gender}.`);
+});
