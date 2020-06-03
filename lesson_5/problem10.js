b147821aae0dd5ce633d0f58e2468e3f7859e4cc
Sean Kuusinen <seankuusinen@gmail.com>
2020-06-03 16:00:52 +0000

    need to remember the logic for returning 1, -1, or 0 when sorting strings

---
+++ lesson_5/problem10.js
@@ -0,0 +1,23 @@
+/* Perform the same transformation of sorting the subarrays we did in the 
+previous exercise with one difference; sort the elements in descending order.*/
+
+let arr = [['b', 'c', 'a'], [2, 1, 3], ['blue', 'black', 'green']];
+
+arr.map(subArr => {
+  return subArr.slice().sort((a, b) => {
+    if (typeof a === 'number') {
+      //if the array is numbers
+      return b-a;
+    }
+    
+    if (a < b) {
+      return 1
+    } else if (a > b) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+});
+
+console.log(subArr);
