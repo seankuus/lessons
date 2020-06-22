8098cfe3a751eeb167b58cd67ab6b894e7c8bc38
Sean Kuusinen <seankuusinen@gmail.com>
2020-06-22 19:07:02 +0000

    Easy 3 #10

---
+++ practice_problems/easy_3/century.js
@@ -0,0 +1,31 @@
+/*Write a function that takes a year as input and returns the century. The 
+return value should be a string that begins with the century number, and ends 
+with 'st', 'nd', 'rd', or 'th' as appropriate for that number.*/
+
+function century(year) {
+  let centuryNumber = Math.floor(year / 100) + 1;
+
+  if (year % 100 === 0) {
+    centuryNumber -= 1;
+  }
+
+  return String(centuryNumber) + centurySuffix(centuryNumber);
+}
+
+function centurySuffix(centuryNumber) {
+  if (catchWithTh(centuryNumber % 100)) {
+    return 'th';
+  }
+
+  let lastDigit = centuryNumber % 10;
+  switch (lastDigit) {
+    case 1: return 'st';
+    case 2: return 'nd';
+    case 3: return 'rd';
+    default: return 'th';
+  }
+}
+
+function catchWithTh(lastTwo) {
+  return lastTwo === 11 || lastTwo === 12 || lastTwo === 13;
+}
