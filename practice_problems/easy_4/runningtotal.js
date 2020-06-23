8e879c852bb09f42dcafe0bd01f93809f570b320
Sean Kuusinen <seankuusinen@gmail.com>
2020-06-23 19:05:18 +0000

    Easy 4 #5

---
+++ practice_problems/easy_4/runningtotal.js
@@ -0,0 +1,24 @@
+/*Write a function that takes an array of numbers, and returns an array with the
+same number of elements, with each element's value being the running total from
+the original array.*/
+
+//D: Array 
+
+//A: input with an array, map new array with call back adding them together
+
+function runningTotal(array) {
+  let runningTotalArray = [];
+  let totaler = 0;
+  
+  for (let i = 0; i < array.length; i++) {
+    runningTotalArray.push((totaler += array[i]));
+  }
+  
+  return runningTotalArray;
+}
+
+//tests
+console.log(runningTotal([2, 5, 13]));
+console.log(runningTotal([14, 11, 7, 15, 20]));
+console.log(runningTotal([3]));
+console.log(runningTotal([]));
